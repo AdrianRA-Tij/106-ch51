d0e0bdbfd0b6f74aeab8f215c3357c6ed7fe89d4
AdrianRA-Tij <adrian.rodriguez@tectijuana.edu.mx>
2024-09-23 19:35:49 -0700

    106-s1

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+
+function Hello() {
+console.log("Hello there!");
+}
+let global = 0;
+function init() 
+{S
+    Hello();
+    let temp = 1;
+    global = temp;
+    console.log("hello we are talking about the DOM");
+}
+
+// variable scope
+let i = 10;
+
+for(let i = 0; i < 10; i++) {
+    // empty variable 
+}
+window.onload = init;
